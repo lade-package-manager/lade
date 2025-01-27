57c0d56732698e52eb8efebd6e189a6f468eb03f
TundraClimate <tundra.tundere@gmail.com>
2025-01-27 23:35:10 +0900

    ♻️refactor: Improved crate::exe_name.rs

--- src/exe_name.rs
+++ src/exe_name.rs
@@ -4,10 +4,12 @@ use crate::{error, paths::lade_build_path};
 
 pub fn get_exec_name() -> String {
     let path = lade_build_path().join("exec_name");
-    if path.exists() {
-        return fs::read_to_string(path).unwrap_or_else(|_| {
-            error!("Failed to read exec_name", "Failed to read exec_name");
-        });
+
+    if !path.exists() {
+        return "".to_string();
     }
-    "".to_string()
+
+    fs::read_to_string(path).unwrap_or_else(|_| {
+        error!("Failed to read exec_name", "Failed to read exec_name");
+    })
 }
