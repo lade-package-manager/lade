c114300b27559e21e8cc52d83019fef84d484d48
小笠原 楓人 <fucho.021000@gmail.com>
2025-01-28 09:35:05 +0900

    setup-for-linux.sh の更新

--- setup-for-linux.sh
+++ setup-for-linux.sh
@@ -2,4 +2,6 @@ mkdir -p "$HOME/.config/lade/packagelist"
 mkdir -p "$HOME/.config/lade/logs"
 mkdir -p "$HOME/.config/lade/bin"
 mkdir -p "$HOME/.config/lade/packages"
+mkdir -p "$HOME/.config/lade/cache"
 
+echo "The setup has been successfully completed. Please run 'cargo build --release' before starting work."
