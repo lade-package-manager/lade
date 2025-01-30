a093b008db7d164b989ab29762462cf3bd706a09
小笠原 楓人 <fucho.021000@gmail.com>
2025-01-30 21:58:01 +0900

    Update setup-for-linux.sh

--- setup-for-linux.sh
+++ setup-for-linux.sh
@@ -1,7 +1,7 @@
 mkdir -p "$HOME/.config/lade/packagelists"
 mkdir -p "$HOME/.config/lade/logs"
 mkdir -p "$HOME/.config/lade/bin"
-mkdir -p "$HOME/.config/lade/packages"
+mkdir -p "$HOME/.config/lade/installed"
 mkdir -p "$HOME/.config/lade/cache"
 
 
