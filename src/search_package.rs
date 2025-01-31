8aeec1c26193d2a6a93d1b5354ab552d4190f82e
TundraClimate <tundra.tundere@gmail.com>
2025-01-31 20:58:06 +0900

    ♻️refactor: Improved search_package.rs

--- src/search_package.rs
+++ src/search_package.rs
@@ -16,60 +16,66 @@ pub fn search_package_lade(package: &str) -> Option<LadePackage> {
     ];
 
     for package_list_path in package_list_paths {
-        if package_list_path.exists() {
-            // ZIPファイルを開く
-            let file = fs::File::open(&package_list_path).unwrap_or_else(|e| {
+        if !package_list_path.exists() {
+            continue;
+        }
+
+        // ZIPファイルを開く
+        let file = fs::File::open(&package_list_path).unwrap_or_else(|e| {
+            error!(
+                format!(
+                    "Failed to open {}. Please update lade package list",
+                    package_list_path.file_name().unwrap().to_str().unwrap()
+                ),
+                format!(
+                    "Failed to open {}: {}",
+                    package_list_path.file_name().unwrap().to_str().unwrap(),
+                    e
+                ),
+                404
+            );
+        });
+
+        let reader = BufReader::new(file);
+        let mut archive = ZipArchive::new(reader).unwrap_or_else(|e| {
+            error!(
+                "Failed to unzip archive",
+                format!("Failed to unzip archive: {}", e)
+            );
+        });
+
+        let target_path = format!("{}/info.toml", package);
+
+        // ZIP内のファイルを探索
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap_or_else(|e| {
                 error!(
-                    format!(
-                        "Failed to open {}. Please update lade package list",
-                        package_list_path.file_name().unwrap().to_str().unwrap()
-                    ),
-                    format!(
-                        "Failed to open {}: {}",
-                        package_list_path.file_name().unwrap().to_str().unwrap(),
-                        e
-                    ),
-                    404
+                    "Failed to read archive",
+                    format!("Failed to read archive: {}", e)
                 );
             });
 
-            let reader = BufReader::new(file);
-            let mut archive = ZipArchive::new(reader).unwrap_or_else(|e| {
-                error!(
-                    "Failed to unzip archive",
-                    format!("Failed to unzip archive: {}", e)
-                );
-            });
-
-            let target_path = format!("{}/info.toml", package);
-
-            // ZIP内のファイルを探索
-            for i in 0..archive.len() {
-                let mut file = archive.by_index(i).unwrap_or_else(|e| {
+            if file
+                .enclosed_name()
+                .filter(|p| p.to_string_lossy() == target_path)
+                .is_some()
+            {
+                let mut contents = String::new();
+                file.read_to_string(&mut contents).unwrap_or_else(|e| {
                     error!(
-                        "Failed to read archive",
-                        format!("Failed to read archive: {}", e)
+                        "Failed to read info.toml",
+                        format!("Failed to read info.toml: {}", e)
                     );
                 });
 
-                if file.name() == target_path {
-                    let mut contents = String::new();
-                    file.read_to_string(&mut contents).unwrap_or_else(|e| {
-                        error!(
-                            "Failed to read info.toml",
-                            format!("Failed to read info.toml: {}", e)
-                        );
-                    });
+                let toml = toml::from_str(&contents).unwrap_or_else(|e| {
+                    error!(
+                        "Failed to parse info.toml",
+                        format!("Failed to parse info.toml: {}", e)
+                    );
+                });
 
-                    let toml = toml::from_str(&contents).unwrap_or_else(|e| {
-                        error!(
-                            "Failed to parse info.toml",
-                            format!("Failed to parse info.toml: {}", e)
-                        );
-                    });
-
-                    return Some(toml);
-                }
+                return Some(toml);
             }
         }
     }
